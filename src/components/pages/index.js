501aaac3ffc98c60bd6edf3b2e7137c9533a8eee
Eman59 <sayyedeman59@gmail.com>
2022-03-31 02:43:17 +0530

    unused commented

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,7 +1,7 @@
-import Home01 from './Home01';
+// import Home01 from './Home01';
 import Home02 from './Home02';
-import Home03 from './Home03';
-import Home04 from './Home04';
+// import Home03 from './Home03';
+// import Home04 from './Home04';
 import Blog from './Blog';
 import BlogSingle from './BlogSingle';
 
