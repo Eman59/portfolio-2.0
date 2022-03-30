daba8796bc6340ca0f001666a5873730d120782e
Eman59 <sayyedeman59@gmail.com>
2022-03-31 02:36:44 +0530

    initial commit

---
+++ src/components/layouts/home02/education/ContentEducation.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+
+class ContentEducation extends Component {
+    render() {
+        return (
+            <div className="row content-inside">
+                <div className="col-lg-5 col-md-6 col-12 animate-element wow delay5 fadeInUp" data-wow-delay="0.5s">
+                    <div className="experience-box t2 border-corner5 color-s1">
+                        <div className="box-inner">
+                            <h3 className="f-info color-d3">RD National College</h3>
+                            <div className="s-info">Master's of Science in IT</div>
+                            <p>
+                                CGPA 9.87 / 10
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-7 col-md-6 col-12 animate-element wow delay5 fadeInUp" data-wow-delay="0.6s">
+                    <div className="col-right">
+                        <div className="experience-box t2 border-corner5 color-s1">
+                            <div className="box-inner">
+                                <h3 className="f-info color-d3">Bhavan's College</h3>
+                                <div className="s-info">Bachelor's of Science in IT</div>
+                                <p>
+                                    CGPA 6.07 / 10
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-5 col-md-6 col-12 animate-element wow delay5 fadeInUp" data-wow-delay="0.7s">
+                    <div className="experience-box t2 border-corner5 color-s2">
+                        <div className="box-inner">
+                            <h3 className="f-info color-d4">ST Mary's College</h3>
+                            <div className="s-info">HSC Science</div>
+                            <p>
+                                PCMB JEE-80
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-7 col-md-6 col-12 animate-element wow delay5 fadeInUp" data-wow-delay="0.5s">
+                    <div className="col-right">
+                        <div className="experience-box t2 border-corner5 color-s2">
+                            <div className="box-inner">
+                                <h3 className="f-info color-d4">Michael High School</h3>
+                                <div className="s-info">SSC</div>
+                                <p>
+                                    Percentage - 78.60%
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-5 col-md-6 col-12 animate-element wow delay5 fadeInUp" data-wow-delay="0.6s">
+                    <div className="experience-box t2 border-corner5 color-s3">
+                        <div className="box-inner">
+                            <h3 className="f-info color-d5">EvolutionCo</h3>
+                            <div className="s-info">Software Developer</div>
+                            <p>
+                                Creating unique and pathbreaking experiences on digital platforms.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-7 col-md-6 col-12 animate-element wow delay5 fadeInUp" data-wow-delay="0.7s">
+                    <div className="col-right">
+                        <div className="experience-box t2 border-corner5 color-s3">
+                            <div className="box-inner">
+                                <h3 className="f-info color-d5">Bed Bath & Beyond</h3>
+                                <div className="s-info">Software Engineer</div>
+                                <p>
+                                    Software services in eCommerce, eBanking and eInsurance domains.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ContentEducation;
